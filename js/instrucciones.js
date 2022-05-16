735ccc28267d0117b580a3c5895bb783833748be
ja-calderon <105570149+ja-calderon@users.noreply.github.com>
2022-05-15 22:26:11 -0300

    se sumo validacion de mayusculas y acentos

--- js/instrucciones.js
+++ js/instrucciones.js
@@ -1,46 +1,53 @@
 
-
 //ajustar altura textarea de cuadro de ingreso de texto segun contenido
-function ajustartextareamensaje(){
+function ajustartextareamensaje() {
   const element = document.getElementById("mensaje");
   element.style.height = element.scrollHeight + "px";
 }
-document.addEventListener('input', ajustartextareamensaje)
+document.addEventListener("input", ajustartextareamensaje);
 //ajuste de altura textarea de salida segun contenido
-function ajustartextarea(){
-          const element = document.getElementById("resultado");
-          element.style.height = element.scrollHeight + "px";
-        }
+function ajustartextarea() {
+  const element = document.getElementById("resultado");
+  element.style.height = element.scrollHeight + "px";
+}
 // copiar
 function copiar() {
   var contentText = document.querySelector("#resultado");
   contentText.select();
-  document.execCommand('copy');
-  alert ("Copiado al portapapeles");
+  document.execCommand("copy");
+  alert("Copiado al portapapeles");
 }
-        
+
 //encriptar
 function codificar() {
   var mensajeoriginal = document.getElementById("mensaje").value;
-  if (mensajeoriginal != "") {
-    var mensajeacifrar;
-    mensajeacifrar = mensajeoriginal.replace(/e/g, "enter");
-    mensajeacifrar = mensajeacifrar.replace(/i/g, "imes");
-    mensajeacifrar = mensajeacifrar.replace(/a/g, "ai");
-    mensajeacifrar = mensajeacifrar.replace(/o/g, "ober");
-    mensajeacifrar = mensajeacifrar.replace(/u/g, "ufat");
-    document.getElementById("resultado").innerHTML = mensajeacifrar;} 
-    else {
-    alert("debe completar algo");
+  var letra = document.getElementById("mensaje").value;
+  if (/[A-Z]/.test(letra) || /[\u00C0-\u017F]/.test(letra)) {
+    alert("Solo se aceptan letras minúsculas y sin acentos");
+  } else {
+    if (mensajeoriginal != "") {
+      var mensajeacifrar;
+      mensajeacifrar = mensajeoriginal.replace(/e/g, "enter");
+      mensajeacifrar = mensajeacifrar.replace(/i/g, "imes");
+      mensajeacifrar = mensajeacifrar.replace(/a/g, "ai");
+      mensajeacifrar = mensajeacifrar.replace(/o/g, "ober");
+      mensajeacifrar = mensajeacifrar.replace(/u/g, "ufat");
+      document.getElementById("resultado").innerHTML = mensajeacifrar;
+      ocultarmensaje();
+      ajustartextarea();
+    } else {
+      alert("debe completar algo");
+    }
   }
-
-ocultarmensaje();
-ajustartextarea();
 }
 
 //descriptar
-function decodificar(){
-    var mensajeoriginal = document.getElementById("mensaje").value;
+function decodificar() {
+  var mensajeoriginal = document.getElementById("mensaje").value;
+  var letra = document.getElementById("mensaje").value;
+  if (/[A-Z]/.test(letra) || /[\u00C0-\u017F]/.test(letra)) {
+    alert("Solo se aceptan letras minúsculas y sin acentos");
+  } else {
     if (mensajeoriginal != "") {
       var mensajeadecifrar;
       mensajeadecifrar = mensajeoriginal.replace(/enter/g, "e");
@@ -48,32 +55,30 @@ function decodificar(){
       mensajeadecifrar = mensajeadecifrar.replace(/ai/g, "a");
       mensajeadecifrar = mensajeadecifrar.replace(/ober/g, "o");
       mensajeadecifrar = mensajeadecifrar.replace(/ufat/g, "u");
-      document.getElementById("resultado").innerHTML = mensajeadecifrar;} 
-      else {
+      document.getElementById("resultado").innerHTML = mensajeadecifrar;
+      ocultarmensaje();
+      ajustartextarea();
+    } else {
       alert("debe completar algo");
     }
-    ocultarmensaje();
-    ajustartextarea();
-
+  }
 }
 //selector de mensajes de salida
-function ocultarmensaje(){
+function ocultarmensaje() {
   const mostrar = document.getElementById("cuadrosalida");
-      if (mostrar.style.visibility == 'hidden') {
-          mostrar.style.visibility = 'visible';
-          mostrar.style.display = 'flex';
-      } else {
-          mostrar.style.visibility = 'visible';
-          mostrar.style.display = 'flex';
-        }
+  if (mostrar.style.visibility == "hidden") {
+    mostrar.style.visibility = "visible";
+    mostrar.style.display = "flex";
+  } else {
+    mostrar.style.visibility = "visible";
+    mostrar.style.display = "flex";
+  }
   const mostrarcuadro = document.getElementById("cuadro-mensaje");
-        if (mostrarcuadro.style.visibility == 'visible') {
-            mostrarcuadro.style.visibility = 'hidden';
-            mostrarcuadro.style.display = 'none';
-        } else {
-            mostrarcuadro.style.visibility = 'hidden';
-            mostrarcuadro.style.display = 'none';
-          }
-        }
-
-
+  if (mostrarcuadro.style.visibility == "visible") {
+    mostrarcuadro.style.visibility = "hidden";
+    mostrarcuadro.style.display = "none";
+  } else {
+    mostrarcuadro.style.visibility = "hidden";
+    mostrarcuadro.style.display = "none";
+  }
+}
